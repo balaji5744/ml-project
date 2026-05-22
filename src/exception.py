08b96dd24bba8b9130117e47be51ec8492da2845
balaji5744 <sayabalaji1@gmail.com>
2026-05-22 17:43:10 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,24 @@
+import sys
+import logging
+
+def error_msg_deatils(error, error_detail: sys):
+    _,_,exc_tb = error_detail.exc_info()
+    filename=exc_tb.tb_frame.f_code.co_filename
+    line_number=exc_tb.tb_lineno
+    error_message = "Error occured in python script: [{0}] line number: [{1}] error message: [{2}]".format(
+        filename, line_number, str(error)
+    )
+    
+    return error_message
+    
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_msg_deatils(error_message, error_detail=error_detail)
+        
+    def __str__(self):
+        return self.error_message
+    
+    
+
+    
